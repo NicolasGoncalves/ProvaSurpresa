4ed396abfa27b0481c8b0b034464997990b3940d
jonaass <jonascn.2021@gmail.com>
2022-10-26 18:18:57 -0300

    14 feita

--- src/pages/14.Escola/index.js
+++ src/pages/14.Escola/index.js
@@ -1,43 +1,36 @@
 import { useEffect, useState } from 'react';
-import escola from '../../services/Escola';
+
+import escola from '../../services/Escola/index.js';
+import array from '../../services/Escola/array.js';
+
 import './index.js';
 
-
-
 export default function Escola() {
-
-
     const [QTD, setQTD] = useState(0);
     const [notas, setNotas] = useState([]);
     const [resp, setResp] = useState([]);
 
-
-    // const [tamanho, setTamanho] = useState(0)
-    // const [inputs, setInputs] = useState([])
-
-
-
-    function alterarInputs() {
-        const r = QTD;
-        setNotas(r);
-    }
-
-
-
     function Calcular() {
-
         try {
             const r = escola(notas);
             setResp(r);
-
         }
         catch (err) {
             alert(err.message)
         }
     }
 
+    function Mapear() {
+        const x = array(QTD-1);
+        setNotas(x);
+    }
+
+    function alterar(pos, novo) {
+        notas[pos] = Number(novo);
+        setNotas([...notas]);
+    }
+
     useEffect(() => {
-        // alterarInputs();
         Calcular();
     }, []);
 
@@ -56,14 +49,34 @@ export default function Escola() {
 
                     <label>Quantidade de alunos </label>
                     <input type="number" value={QTD} onChange={e => setQTD(Number(e.target.value))} />
-                
+
+                </div>
+
+                <div>
+                <button onClick={Mapear}> ok </button>
                 </div>
 
                 <div className="maps">
-                    {notas.map(item=>{
-                        
-                        <input type="number" />
-                    })}
+                    {notas.map((item, pos) =>
+                        <div>
+                            Aluno {pos+1}: <input type='text' value={notas[pos]} onChange={e => alterar(pos, e.target.value)} />
+                        </div>
+                    )}
+                </div>
+
+                <div>
+                    <button onClick={Calcular}> Calcular </button>
+                </div>
+
+                <div>
+                    <label>Média </label>
+                    <input type="number" value={resp[0]} />
+                    
+                    <label>Maior </label>
+                    <input type="number" value={resp[1]} />
+                    
+                    <label>Menor </label>
+                    <input type="number" value={resp[2]} />
                 </div>
 
             </section>
---
+++ src/services/Escola/array.js
@@ -0,0 +1,7 @@
+export default function array(tam){
+    let x=[];
+    for(let i=0;i<=tam;i++){
+        x.push(0)
+    }
+    return x;
+}
